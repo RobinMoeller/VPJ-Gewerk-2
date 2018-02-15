d506ec395b04845963a35ed01679233864899c35
JanGellermann <34571456+JanGellermann@users.noreply.github.com>
2018-02-15 14:11:03 +0100

    Ausweichgedöns
    Fehler bei ändern des Vorzeichens vom Winkel.

---
+++ Untitled.m
@@ -0,0 +1,8 @@
+plot(Vektor(2,:),Vektor(3,:),'*');
+hold on;
+plot(Vektor(5,:),Vektor(6,:),'*');
+plot(Vektor(8,:),Vektor(9,:),'*');
+plot(Vektor(11,:),Vektor(12,:),'*');
+plot(Vektor(14,:),Vektor(15,:),'*');
+hold off;
+grid on;
